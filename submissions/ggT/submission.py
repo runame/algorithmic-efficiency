5fbe71676fdd392427718ad64d2de98c4915842f
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-04 15:52:00 -0400

    [FIX] Batch size function for all workloads

--- submissions/ggT/submission.py
+++ submissions/ggT/submission.py
@@ -1,4 +1,5 @@
-from typing i
+from typing
+i
 mport Iterator
 
 from absl import logging
@@ -13,10 +14,13 @@ from algorithmic_efficiency.pytorch_utils import pytorch_setup
 from submissions.ggT.optimizer import LocalOptimizer_GGT
 from algorithmic_efficiency.workloads.ogbg.ogbg_pytorch.workload import OgbgWorkload
 from algorithmic_efficiency.workloads.wmt.wmt_pytorch.workload import WmtWorkload
-from algorithmic_efficiency.workloads/imagenet_resnet/imagenet_pytorch/workload import ImagenetResNetWorkload, ImagenetResNetSiLUWorkload, ImagenetResNetGELUWorkload, ImagenetResNetLargeBNScaleWorkload
-from algorithmic_efficiency.workloads.imagenet_vit.imagenet_pytorch.workload import ImagenetVitWorkload, ImagenetVitGluWorkload, ImagenetVitPostLNWorkload, ImagenetVitMapWorkload
-from algorithmic_efficiency.workloads.librispeech_conformer.librispeech_pytorch.workload import LibriSpeechConformerWorkload, LibriSpeechConformerAttentionTemperatureWorkload, LibriSpeechConformerLayerNormWorkload, LibriSpeechConformerGeluWorkload
-from algorithmic_efficiency.workloads.librispeech_deepspeech.librispeech_pytorch.workload import LibriSpeechDeepSpeechWorkload, LibriSpeechDeepSpeechTanhWorkload, LibriSpeechDeepSpeechNoResNetWorkload, LibriSpeechDeepSpeechNormAndSpecAugWorkload
+from algorithmic_efficiency.workloads/imagenet_resnet/imagenet_pytorch/workload import ImagenetResNetWorkload
+from algorithmic_efficiency.workloads.imagenet_vit.imagenet_pytorch.workload import ImagenetVitWorkload
+from algorithmic_efficiency.workloads.librispeech_conformer.librispeech_pytorch.workload import LibriSpeechConformerWorkload
+from algorithmic_efficiency.workloads.librispeech_deepspeech.librispeech_pytorch.workload import LibriSpeechDeepSpeechWorkload
+from algorithmic_efficiency.workloads.criteo1tb.criteo1tb_pytorch.workload import Criteo1TbDlrmSmallWorkload
+from algorithmic_efficiency.workloads.fastmri.fastmri_pytorch.workload import FastMRIWorkload
+from algorithmic_efficiency.workloads.mnist.mnist_pytorch.workload import MnistWorkload
 
 USE_PYTORCH_DDP = pytorch_setup()[0]
 
@@ -148,14 +152,20 @@ def get_batch_size(workload_name):
           return get_batch_size('ogbg')
       elif isinstance(workload_name, WmtWorkload):
           return get_batch_size('wmt')
-      elif isinstance(workload_name, (ImagenetResNetWorkload, ImagenetResNetSiLUWorkload, ImagenetResNetGELUWorkload, ImagenetResNetLargeBNScaleWorkload)):
+      elif isinstance(workload_name, ImagenetResNetWorkload):
           return get_batch_size('imagenet_resnet')
-      elif isinstance(workload_name, (ImagenetVitWorkload, ImagenetVitGluWorkload, ImagenetVitPostLNWorkload, ImagenetVitMapWorkload)):
+      elif isinstance(workload_name, ImagenetVitWorkload):
           return get_batch_size('imagenet_vit')
-      elif isinstance(workload_name, (LibriSpeechConformerWorkload, LibriSpeechConformerAttentionTemperatureWorkload, LibriSpeechConformerLayerNormWorkload, LibriSpeechConformerGeluWorkload)):
+      elif isinstance(workload_name, LibriSpeechConformerWorkload):
           return get_batch_size('librispeech_conformer')
-      elif isinstance(workload_name, (LibriSpeechDeepSpeechWorkload, LibriSpeechDeepSpeechTanhWorkload, LibriSpeechDeepSpeechNoResNetWorkload, LibriSpeechDeepSpeechNormAndSpecAugWorkload)):
+      elif isinstance(workload_name, LibriSpeechDeepSpeechWorkload):
           return get_batch_size('librispeech_deepspeech')
+      elif isinstance(workload_name, Criteo1TbDlrmSmallWorkload):
+          return get_batch_size('criteo1tb')
+      elif isinstance(workload_name, FastMRIWorkload):
+          return get_batch_size('fastmri')
+      elif isinstance(workload_name, MnistWorkload):
+          return get_batch_size('mnist')
       else:
           raise ValueError
   # Return the global batch size.
