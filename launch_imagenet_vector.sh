25c1813b97c70d138e2289ec16640a939b217237
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-07-12 11:50:57 -0400

    [ADD] Launch scripts for fastmri, imagenet, wmt

--- launch_imagenet_vector.sh
+++ launch_imagenet_vector.sh
@@ -8,15 +8,15 @@
 
 # NOTE1 Before being able to run this, you need to carry out the data preparation:
 # 1) Create a folder `data`: `mkdir data`
-# 2) Download the data set: `python3 datasets/dataset_setup.py --data_dir data/ --ogbg`
+# 2) Link ImageNet: `ln -s /datasets/imagenet/ ./data/imagenet`
 #
 # NOTE2 To use more GPUs change `--gres=gpu:X` above, and `nproc_per_node=X` to a larger number
 torchrun --redirects 1:0,2:0,3:0,4:0,5:0,6:0,7:0 --standalone --nnodes=1 --nproc_per_node=2 submission_runner.py \
     --framework=pytorch \
-    --data_dir=data/ogbg/ \
+    --data_dir=data/imagenet/ \
     --experiment_dir=experiments/ \
-    --experiment_name=ogbg_first_try \
-    --workload=ogbg \
+    --experiment_name=imagenet_vit_first_try \
+    --workload=imagenet_vit \
     --submission_path=submissions/submission_folder/external_tuning/sirfshampoo/submission.py \
     --tuning_search_space=submissions/submission_folder/external_tuning/sirfshampoo/tuning_search_space.json \
     --overwrite=True  \
